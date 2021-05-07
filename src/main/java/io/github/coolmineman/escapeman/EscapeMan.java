ebd4b1819cb48a884b14f9e6fa58fbe2b653b7b3
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-05-07 10:10:09 -0500

    6/7

---
+++ src/main/java/io/github/coolmineman/escapeman/EscapeMan.java
@@ -0,0 +1,117 @@
+package io.github.coolmineman.escapeman;
+
+import java.nio.IntBuffer;
+import java.util.Map;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.APIUtil;
+import org.lwjgl.system.MemoryStack;
+import org.pmw.tinylog.Configurator;
+import org.pmw.tinylog.Logger;
+
+public class EscapeMan {
+    private long window;
+
+    public void run() {
+        Logger.info("Starting EscapeMan");
+        Logger.info("LWGL Version " + Version.getVersion());
+        init();
+        gameLoop();
+    }
+
+    @SuppressWarnings("all")
+    private void init() {
+        //Setup GLFW (The window creation library) Errors to Get Logged
+        //Based on GLFWErrorCallback.createPrint
+        GLFWErrorCallback.create(
+            new GLFWErrorCallback() {
+                private Map<Integer, String> ERROR_CODES = APIUtil.apiClassTokens((field, value) -> 0x10000 < value && value < 0x20000, null, GLFW.class);
+
+                @Override
+                public void invoke(int error, long description) {
+                    String msg = getDescription(description);
+
+                    Logger.error("[LWJGL] " + ERROR_CODES.get(error) + " error");
+                    Logger.error("\tDescription: " + msg);
+                    Logger.error("\tStacktrace:");
+                    StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+                    for ( int i = 4; i < stack.length; i++ ) {
+                        Logger.error("\t\t" + stack[i].toString());
+                    }
+                }
+            }
+        ).set();
+        
+        if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(1600, 900, "EscapeMan", NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        glfwSetKeyCallback(window, this::keyCallback);
+
+        try (MemoryStack stack = stackPush()) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		}
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1); //v-sync
+        glfwShowWindow(window);
+    }
+
+    private void gameLoop() {
+        GL.createCapabilities();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //black
+
+		while (!glfwWindowShouldClose(window)) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            //TODO render stuff here
+
+			glfwSwapBuffers(window);
+
+			glfwPollEvents(); // Polls input events and call the callbacks
+		}
+    }
+
+    public void keyCallback(long keywindow, int key, int scancode, int action, int mods) {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
+    public static void main(String[] args) {
+        Configurator.defaultConfig().writingThread(true).formatPattern("{date} [{thread}] {level}: {message}").activate();
+        (new EscapeMan()).run();
+    }
+}
